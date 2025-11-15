62badcd0a69be9aa208c5f899f22deb675a9523e
Denyol18 <sprokdaniel@gmail.com>
2025-11-15 18:10:02 +0100

    i lied

--- app.tf
+++ app.tf
@@ -80,7 +80,7 @@ resource "docker_container" "prometheus" {
 
   mounts {
       target = "/etc/prometheus/prometheus.yml"
-      source = ${path.cwd}/prometheus.yml
+      source = "${path.cwd}/prometheus.yml"
       type   = "bind"
   }
 
